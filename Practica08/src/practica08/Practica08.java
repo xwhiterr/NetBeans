5348a79e246154a19b28658f9d2b05ba509db2fd
xwhiterr <aquinojonathan37@gmail.com>
2023-12-22 20:11:50 -0300

    Añadir Proyecto 7 y 8

---
+++ Practica08/src/practica08/Practica08.java
@@ -0,0 +1,16 @@
+package practica08;
+
+/**
+ *
+ * @author Walter Aquino
+ */
+public class Practica08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
